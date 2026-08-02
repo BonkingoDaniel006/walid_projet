cfdd436018f5610b24f10b9557c46ca7f65e9c94
Daniel Bokingo Botay <bokingodaniel4@gmail.com>
2026-08-02 21:46:05 +0200

    modification de la structure du json attendu par l'api

--- app.py
+++ app.py
@@ -3,43 +3,42 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 # Variables globales pour stocker temporairement les données
-nom = None
-age = None
-
+intrusion = False
 @app.route('/api/data', methods=['POST'])
 def receive_data():
-    global nom, age
+    global intrusion
     # Récupération des données JSON envoyées dans le corps de la requête
     data = request.get_json()
     
     if not data:
         return jsonify({"status": "error", "message": "Aucun corps JSON fourni"}), 400
     
-    nom = data.get('nom')
-    age = data.get('age')
+    intrusion = data.get('intrusion')
     
-    print(f"Données reçues sur le serveur - Nom: {nom}, Âge: {age}")
+    
+    
+    print(f"Données reçues sur le serveur -  etat: {intrusion}")
     
     # Traitement ou sauvegarde des données ici...
     
     return jsonify({
         "status": "success",
         "message": "Données bien enregistrées !",
-        "recu": {"nom": nom, "age": age}
+        "recu": {"intrusion": intrusion}
     }), 200
 
 
 @app.route("/home")
 def home():
-    return render_template("index.html", age=age, nom=nom)
+    return render_template("index.html", message = "intrusion dans la piece detectez merci d'envoyer des renforts.")
+    
 
 
 # Nouvelle route pour alimenter le script JS du dashboard en direct
 @app.route('/api/last-data', methods=['GET'])
 def get_last_data():
     return jsonify({
-        "nom": nom,
-        "age": age
+        "intrusion": intrusion
     }), 200
 
 
